396febff62ed5bc1eb1264f4a0f1d5a7cadaee9c
Iona Erofeeff <37254166+ionarevamp@users.noreply.github.com>
2023-10-21 19:26:41 +0000

    Update bypass.c

--- src/lib/bypass.c
+++ src/lib/bypass.c
@@ -10,11 +10,9 @@ void sleep_s(float x) {
   usleep((int)(x * 1000 * 1000));
 }
 void rgbwr(const char* text,int r,int g,int b) {
-<<<<<<< HEAD
   r = (255*(r>255))+(r*(r<255))
   g = (255*(g>255))+(g*(r<255))
   b = (255*(b>255))+(r*(b<255))
 =======
->>>>>>> refs/remotes/origin/master
   printf("\33[38;2;%d;%d;%dm%s\33[0m",(int)r,(int)g,(int)b,text);
 }
